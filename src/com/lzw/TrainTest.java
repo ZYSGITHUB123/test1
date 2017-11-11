f6c0048cf0511d75bc0b6e842f7212c7f6f64ace
ZYS <ZYS839345426@163.comm>
2017-11-11 19:30:46 +0800

    message

---
+++ src/com/lzw/TrainTest.java
@@ -0,0 +1,15 @@
+package com.lzw;
+
+class A{
+      public A(){
+    	  System.out.println("1111");
+      }
+	
+
+}
+public class TrainTest{
+	public static void main(String[] args){
+	A a=new A();
+	}
+	
+}
